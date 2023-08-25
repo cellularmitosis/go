5b74883c650a041d8d97dcc10844e0cc4cd28006
Jason Pepas <jasonpepas@gmail.com>
2023-08-24 23:14:59 -0500

    darwin9

---
+++ build-darwin9-amd64.sh
@@ -0,0 +1,21 @@
+#!/bin/bash
+
+set -e -o pipefail -x
+
+dest=go-1.5.4-darwin9-amd64
+
+cd src
+GOROOT_BOOTSTRAP=$HOME/opt/go-1.4.3-darwin9-amd64 \
+CGO_CFLAGS=-mmacosx-version-min=10.5 \
+CGO_LDFLAGS=-mmacosx-version-min=10.5 \
+./make.bash
+cd ..
+
+src=$PWD
+cd ..
+mkdir -p ~/opt
+rm -rf $dest
+rsync -a --exclude='.git/' $src ~/opt/$dest
+
+cd ~/opt
+tar c $dest | gzip -9 > /tmp/$dest.tar.gz
